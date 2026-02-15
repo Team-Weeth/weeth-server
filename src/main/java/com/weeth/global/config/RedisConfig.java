16b4754db491b97f4b6f5d0c49ff933a9aa28b06
hyxklee <ewgt1234@naver.com>
2026-02-15 12:35:25 +0900

    chore: sas 관련 redis 설정 제거

--- src/main/java/com/weeth/global/config/RedisConfig.java
+++ src/main/java/com/weeth/global/config/RedisConfig.java
@@ -1,7 +1,5 @@
 package com.weeth.global.config;
 
-// TODO: SAS converters need to be migrated - commented out temporarily
-// import com.weeth.global.sas.application.converter.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +8,8 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.convert.RedisCustomConversions;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP) // redis index ttl 설정
@@ -40,18 +35,6 @@ public class RedisConfig {
         return new LettuceConnectionFactory(redisConfiguration);
     }
 
-    // TODO: Re-enable after SAS converters are migrated
-    // @Bean
-    // public RedisCustomConversions redisCustomConversions() {
-    //     return new RedisCustomConversions(
-    //             Arrays.asList(
-    //             new UsernamePasswordAuthenticationTokenToBytesConverter(),
-    //             new BytesToUsernamePasswordAuthenticationTokenConverter(),
-    //             new OAuth2AuthorizationRequestToBytesConverter(),
-    //             new BytesToOAuth2AuthorizationRequestConverter(),
-    //             new ClaimsHolderToBytesConverter(),
-    //             new BytesToClaimsHolderConverter()));
-    // }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate() {
