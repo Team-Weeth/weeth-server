a9a8be2f1eb47c633298883cc83822c7559cf21b
hyxklee <ewgt1234@naver.com>
2026-02-15 18:18:27 +0900

    chore: ConfigurationProperty 스캔 설정

--- src/main/java/com/weeth/WeethApplication.java
+++ src/main/java/com/weeth/WeethApplication.java
@@ -1,9 +1,8 @@
 package com.weeth;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,6 +16,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableJpaAuditing
 @EnableWebSecurity
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class WeethApplication {
 
     public static void main(String[] args) {
