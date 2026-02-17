f095fd545577ee31e9121d450c80135e635d1b2e
hyxklee <ewgt1234@naver.com>
2026-02-17 15:36:10 +0900

    refactor: File 도메인 변경에 따른 기존 코드 수정

--- src/test/kotlin/com/weeth/domain/file/fixture/FileTestFixture.kt
+++ src/test/kotlin/com/weeth/domain/file/fixture/FileTestFixture.kt
@@ -1,20 +1,25 @@
 package com.weeth.domain.file.fixture
 
-import com.weeth.domain.board.domain.entity.Notice
 import com.weeth.domain.file.domain.entity.File
+import com.weeth.domain.file.domain.entity.FileOwnerType
 
 object FileTestFixture {
     fun createFile(
         id: Long,
         fileName: String,
-        fileUrl: String,
-        notice: Notice? = null,
+        storageKey: String = "NOTICE/2026-02/test.png",
+        fileSize: Long = 1024,
+        ownerType: FileOwnerType = FileOwnerType.NOTICE,
+        ownerId: Long = 1L,
+        contentType: String = "image/png",
     ): File =
-        File
-            .builder()
-            .id(id)
-            .fileName(fileName)
-            .fileUrl(fileUrl)
-            .notice(notice)
-            .build()
+        File(
+            id = id,
+            fileName = fileName,
+            storageKey = storageKey,
+            fileSize = fileSize,
+            ownerType = ownerType,
+            ownerId = ownerId,
+            contentType = contentType,
+        )
 }
