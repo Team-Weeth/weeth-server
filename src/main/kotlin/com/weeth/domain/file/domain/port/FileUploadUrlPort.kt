94671b03ea1456fc94d1ca214e1ae00809c042a1
hyxklee <ewgt1234@naver.com>
2026-02-17 16:21:00 +0900

    refactor: 주석 추가

--- src/main/kotlin/com/weeth/domain/file/domain/port/FileUploadUrlPort.kt
+++ src/main/kotlin/com/weeth/domain/file/domain/port/FileUploadUrlPort.kt
@@ -2,6 +2,7 @@ package com.weeth.domain.file.domain.port
 
 import com.weeth.domain.file.domain.entity.FileOwnerType
 
+/** [FileUploadUrlPort.generateUploadUrl] 반환 타입 */
 data class FileUploadUrl(
     val fileName: String,
     val storageKey: String,
