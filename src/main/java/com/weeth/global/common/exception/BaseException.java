738f874886034111892ab1d491c1e39b16d4b734
hyxklee <ewgt1234@naver.com>
2026-02-17 15:39:31 +0900

    refactor: 예외 발생 원인 유지

--- src/main/java/com/weeth/global/common/exception/BaseException.java
+++ src/main/java/com/weeth/global/common/exception/BaseException.java
@@ -8,15 +8,19 @@ public abstract class BaseException extends RuntimeException {
     private final int statusCode;
     private final ErrorCodeInterface errorCode;
 
-    public BaseException(int code, String message) {
-        super(message);
+    public BaseException(int code, String message, Throwable cause) {
+        super(message, cause);
         this.statusCode = code;
         this.errorCode = null;
     }
 
-    public BaseException(ErrorCodeInterface errorCode) {
-        super(errorCode.getMessage());
+    public BaseException(ErrorCodeInterface errorCode, Throwable cause) {
+        super(errorCode.getMessage(), cause);
         this.statusCode = errorCode.getStatus().value();
         this.errorCode = errorCode;
+    }
+
+    public BaseException(ErrorCodeInterface errorCode) {
+        this(errorCode, null);
     }
 }
