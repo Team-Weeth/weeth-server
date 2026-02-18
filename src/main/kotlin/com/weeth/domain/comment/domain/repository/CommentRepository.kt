cfadd16d1b863ba7e8f0b6532705a5e198ddc92d
hyxklee <ewgt1234@naver.com>
2026-02-18 22:16:25 +0900

    refactor: Comment 변경 사항 반영

--- src/main/kotlin/com/weeth/domain/comment/domain/repository/CommentRepository.kt
+++ src/main/kotlin/com/weeth/domain/comment/domain/repository/CommentRepository.kt
@@ -3,14 +3,9 @@ package com.weeth.domain.comment.domain.repository
 import com.weeth.domain.comment.domain.entity.Comment
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CommentRepository : JpaRepository<Comment, Long> {
+interface CommentRepository : JpaRepository<Comment, Long>, CommentReader {
     fun findByIdAndPostId(
         id: Long,
         postId: Long,
-    ): Comment?
-
-    fun findByIdAndNoticeId(
-        id: Long,
-        noticeId: Long,
     ): Comment?
 }
