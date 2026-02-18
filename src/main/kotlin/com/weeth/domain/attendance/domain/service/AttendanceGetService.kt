4503684ae3592ba09dc9f2e6e3c14fbf6b9831be
soo0711 <k300hys@naver.com>
2026-02-18 14:33:34 +0900

    refactor: AttendanceGetService 미사용 메서드 제거

--- src/main/kotlin/com/weeth/domain/attendance/domain/service/AttendanceGetService.kt
+++ src/main/kotlin/com/weeth/domain/attendance/domain/service/AttendanceGetService.kt
@@ -1,11 +1,9 @@
 package com.weeth.domain.attendance.domain.service
 
-import com.weeth.domain.attendance.application.exception.AttendanceNotFoundException
 import com.weeth.domain.attendance.domain.entity.Attendance
 import com.weeth.domain.attendance.domain.repository.AttendanceRepository
 import com.weeth.domain.schedule.domain.entity.Meeting
 import com.weeth.domain.user.domain.entity.enums.Status
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,8 +11,4 @@ class AttendanceGetService(
     private val attendanceRepository: AttendanceRepository,
 ) {
     fun findAllByMeeting(meeting: Meeting): List<Attendance> = attendanceRepository.findAllByMeetingAndUserStatus(meeting, Status.ACTIVE)
-
-    fun findByAttendanceId(attendanceId: Long): Attendance =
-        attendanceRepository.findByIdOrNull(attendanceId)
-            ?: throw AttendanceNotFoundException()
 }
