c69b6fb04bc8ae46a89047e925f1df6a9d24f675
hyxklee <ewgt1234@naver.com>
2026-02-18 22:18:20 +0900

    feat: 글로벌 Json Converter 구현

---
+++ src/main/kotlin/com/weeth/global/common/converter/JsonConverter.kt
@@ -0,0 +1,21 @@
+package com.weeth.global.common.converter
+
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import jakarta.persistence.AttributeConverter
+
+abstract class JsonConverter<T>(
+    private val typeRef: TypeReference<T>,
+) : AttributeConverter<T, String> {
+    companion object {
+        private val objectMapper =
+            ObjectMapper().apply {
+                registerModule(KotlinModule.Builder().build())
+            }
+    }
+
+    override fun convertToDatabaseColumn(attribute: T?): String? = attribute?.let { objectMapper.writeValueAsString(it) }
+
+    override fun convertToEntityAttribute(dbData: String?): T? = dbData?.let { objectMapper.readValue(it, typeRef) }
+}
