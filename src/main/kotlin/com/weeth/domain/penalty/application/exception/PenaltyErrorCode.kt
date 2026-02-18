c3b7cdb09ceefa6374e18a12bace59bc5af1266f
soo0711 <k300hys@naver.com>
2026-02-18 22:57:22 +0900

    refactor: 코틀린 문법에 맞게 Penalty errorcode 수정

--- src/main/kotlin/com/weeth/domain/penalty/application/exception/PenaltyErrorCode.kt
+++ src/main/kotlin/com/weeth/domain/penalty/application/exception/PenaltyErrorCode.kt
@@ -5,14 +5,20 @@ import com.weeth.global.common.exception.ExplainError
 import org.springframework.http.HttpStatus
 
 enum class PenaltyErrorCode(
-    override val code: Int,
-    override val status: HttpStatus,
-    override val message: String,
+    private val code: Int,
+    private val status: HttpStatus,
+    private val message: String,
 ) : ErrorCodeInterface {
-
     @ExplainError("요청한 패널티 ID가 존재하지 않을 때 발생합니다.")
     PENALTY_NOT_FOUND(2600, HttpStatus.NOT_FOUND, "존재하지 않는 패널티입니다."),
 
     @ExplainError("시스템에 의해 자동 부여된 패널티를 수동으로 삭제하려 할 때 발생합니다.")
     AUTO_PENALTY_DELETE_NOT_ALLOWED(2601, HttpStatus.BAD_REQUEST, "자동 생성된 패널티는 삭제할 수 없습니다"),
+    ;
+
+    override fun getCode(): Int = code
+
+    override fun getStatus(): HttpStatus = status
+
+    override fun getMessage(): String = message
 }
