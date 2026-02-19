190330ab66cf63e0724e77e1a52e7bc638010a7d
soo0711 <k300hys@naver.com>
2026-02-19 15:15:27 +0900

    refactor: Account repository kotlin 문법으로 변환

--- src/main/kotlin/com/weeth/domain/account/domain/repository/AccountRepository.kt
+++ src/main/kotlin/com/weeth/domain/account/domain/repository/AccountRepository.kt
@@ -1,13 +1,10 @@
-package com.weeth.domain.account.domain.repository;
+package com.weeth.domain.account.domain.repository
 
-import com.weeth.domain.account.domain.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.weeth.domain.account.domain.entity.Account
+import org.springframework.data.jpa.repository.JpaRepository
 
-import java.util.Optional;
+interface AccountRepository : JpaRepository<Account, Long> {
+    fun findByCardinal(cardinal: Int): Account?
 
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
-    Optional<Account> findByCardinal(Integer cardinal);
-
-    boolean existsByCardinal(Integer cardinal);
+    fun existsByCardinal(cardinal: Int): Boolean
 }
--- src/main/kotlin/com/weeth/domain/account/domain/repository/ReceiptRepository.kt
+++ src/main/kotlin/com/weeth/domain/account/domain/repository/ReceiptRepository.kt
@@ -1,10 +1,8 @@
-package com.weeth.domain.account.domain.repository;
+package com.weeth.domain.account.domain.repository
 
-import com.weeth.domain.account.domain.entity.Receipt;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.weeth.domain.account.domain.entity.Receipt
+import org.springframework.data.jpa.repository.JpaRepository
 
-import java.util.List;
-
-public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
-    List<Receipt> findAllByAccountIdOrderByCreatedAtDesc(Long accountId);
+interface ReceiptRepository : JpaRepository<Receipt, Long> {
+    fun findAllByAccountIdOrderByCreatedAtDesc(accountId: Long): List<Receipt>
 }
