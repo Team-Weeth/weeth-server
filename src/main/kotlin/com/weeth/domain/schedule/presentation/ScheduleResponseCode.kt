577cfde4b3536b8a04470681ea75288215a6ae75
soo0711 <k300hys@naver.com>
2026-02-24 16:38:00 +0900

    refactor: ResponseCode 코드 번호 정리

--- src/main/kotlin/com/weeth/domain/schedule/presentation/ScheduleResponseCode.kt
+++ src/main/kotlin/com/weeth/domain/schedule/presentation/ScheduleResponseCode.kt
@@ -12,6 +12,6 @@ enum class ScheduleResponseCode(
     EVENT_UPDATE_SUCCESS(1701, HttpStatus.OK, "일정이 성공적으로 수정되었습니다."),
     EVENT_DELETE_SUCCESS(1702, HttpStatus.OK, "일정이 성공적으로 삭제되었습니다."),
     EVENT_FIND_SUCCESS(1703, HttpStatus.OK, "일정이 성공적으로 조회되었습니다."),
-    SCHEDULE_MONTHLY_FIND_SUCCESS(1710, HttpStatus.OK, "월별 일정이 성공적으로 조회되었습니다."),
-    SCHEDULE_YEARLY_FIND_SUCCESS(1711, HttpStatus.OK, "연도별 일정이 성공적으로 조회되었습니다."),
+    SCHEDULE_MONTHLY_FIND_SUCCESS(1704, HttpStatus.OK, "월별 일정이 성공적으로 조회되었습니다."),
+    SCHEDULE_YEARLY_FIND_SUCCESS(1705, HttpStatus.OK, "연도별 일정이 성공적으로 조회되었습니다."),
 }
