04a8287e268d7129ebd02b8850cb45a05de0b23c
hyxklee <ewgt1234@naver.com>
2026-03-02 11:19:53 +0900

    refactor: BaseEntity, WeethApplication 코틀린 마이그레이션

---
+++ src/main/kotlin/com/weeth/global/common/entity/BaseEntity.kt
@@ -0,0 +1,23 @@
+package com.weeth.global.common.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    var createdAt: LocalDateTime = LocalDateTime.MIN
+        protected set
+
+    @LastModifiedDate
+    var modifiedAt: LocalDateTime = LocalDateTime.MIN
+        protected set
+}
