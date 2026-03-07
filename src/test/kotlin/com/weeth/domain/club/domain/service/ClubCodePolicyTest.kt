8beae91bcec6c4877373e0a6cbd24d5197990f40
hyxklee <ewgt1234@naver.com>
2026-03-07 17:12:22 +0900

    refactor: 테스트 케이스 추가

--- src/test/kotlin/com/weeth/domain/club/domain/service/ClubCodePolicyTest.kt
+++ src/test/kotlin/com/weeth/domain/club/domain/service/ClubCodePolicyTest.kt
@@ -4,7 +4,7 @@ import com.weeth.domain.club.application.exception.InvalidClubCodeException
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.regex.shouldMatch
+import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.string.shouldHaveLength
 import java.util.UUID
 
@@ -22,7 +22,7 @@ class ClubCodePolicyTest :
         "매번 생성되는 코드는 달라야 한다" {
             val code1 = ClubCodePolicy.generateCode()
             val code2 = ClubCodePolicy.generateCode()
-            assert(code1 != code2)
+            code1 shouldNotBe code2
         }
 
         "초대 코드가 일치하면 검증 성공" {
@@ -39,6 +39,24 @@ class ClubCodePolicyTest :
 
             shouldThrow<InvalidClubCodeException> {
                 ClubCodePolicy.validate(clubCode, providedCode)
+            }
+        }
+
+        "초대 코드는 대소문자가 달라도 검증 성공" {
+            val clubCode = "ABCDEF12-3456-7890-ABCD-EF1234567890"
+            val providedCode = "abcdef12-3456-7890-abcd-ef1234567890"
+
+            shouldNotThrow<InvalidClubCodeException> {
+                ClubCodePolicy.validate(clubCode, providedCode)
+            }
+        }
+
+        "초대 코드는 앞뒤 공백을 제거한 뒤 검증 성공" {
+            val code = ClubCodePolicy.generateCode().uppercase()
+            val providedCode = "  $code  "
+
+            shouldNotThrow<InvalidClubCodeException> {
+                ClubCodePolicy.validate(code, providedCode)
             }
         }
     })
