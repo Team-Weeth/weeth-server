fc434f256a607d574478547c7e3c7607b9129ebf
hyxklee <ewgt1234@naver.com>
2026-03-10 11:44:15 +0900

    refactor: 계좌 생성 메서드 수정

--- src/main/kotlin/com/weeth/domain/account/application/usecase/command/ManageAccountUseCase.kt
+++ src/main/kotlin/com/weeth/domain/account/application/usecase/command/ManageAccountUseCase.kt
@@ -5,6 +5,7 @@ import com.weeth.domain.account.application.exception.AccountExistsException
 import com.weeth.domain.account.domain.entity.Account
 import com.weeth.domain.account.domain.repository.AccountRepository
 import com.weeth.domain.cardinal.domain.repository.CardinalReader
+import com.weeth.domain.club.domain.repository.ClubReader
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -12,11 +13,21 @@ import org.springframework.transaction.annotation.Transactional
 class ManageAccountUseCase(
     private val accountRepository: AccountRepository,
     private val cardinalReader: CardinalReader,
+    private val clubReader: ClubReader,
 ) {
+    // TODO(PR4): 해당 클럽 소속 admin인지 검증 필요
     @Transactional
-    fun save(request: AccountSaveRequest) {
-        if (accountRepository.existsByCardinal(request.cardinal)) throw AccountExistsException()
-        cardinalReader.getByCardinalNumber(request.cardinal)
-        accountRepository.save(Account.create(request.description, request.totalAmount, request.cardinal))
+    fun save(
+        clubId: Long,
+        request: AccountSaveRequest,
+    ) {
+        val club = clubReader.getClubById(clubId)
+
+        if (accountRepository.existsByClubIdAndCardinal(clubId, request.cardinal)) throw AccountExistsException()
+
+        cardinalReader.findByClubIdAndCardinalNumber(clubId, request.cardinal)
+            ?: cardinalReader.getByCardinalNumber(request.cardinal) // clubId 기반 조회 실패 시 기존 방식으로 폴백
+
+        accountRepository.save(Account.create(club, request.description, request.totalAmount, request.cardinal))
     }
 }
