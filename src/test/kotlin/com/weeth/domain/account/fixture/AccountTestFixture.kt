3018c897dca98dfc8cef0d5f0802275a184236c9
hyxklee <ewgt1234@naver.com>
2026-03-10 10:17:53 +0900

    refactor: Account 도메인 클럽 추가

--- src/test/kotlin/com/weeth/domain/account/fixture/AccountTestFixture.kt
+++ src/test/kotlin/com/weeth/domain/account/fixture/AccountTestFixture.kt
@@ -1,16 +1,20 @@
 package com.weeth.domain.account.fixture
 
 import com.weeth.domain.account.domain.entity.Account
+import com.weeth.domain.club.domain.entity.Club
+import com.weeth.domain.club.fixture.ClubTestFixture
 
 object AccountTestFixture {
     fun createAccount(
         id: Long = 1L,
+        club: Club = ClubTestFixture.createClub(),
         description: String = "2024년 2학기 회비",
         totalAmount: Int = 100_000,
         currentAmount: Int = 100_000,
         cardinal: Int = 40,
     ): Account =
         Account(
+            club = club,
             id = id,
             description = description,
             totalAmount = totalAmount,
