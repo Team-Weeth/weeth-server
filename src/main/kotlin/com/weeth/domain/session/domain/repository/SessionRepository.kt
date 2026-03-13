4e84489b3de830a403c1b87eb420b576a24143ef
soo0711 <k300hys@naver.com>
2026-03-13 14:03:18 +0900

    refactor: 불필요한 쿼리문 삭제

--- src/main/kotlin/com/weeth/domain/session/domain/repository/SessionRepository.kt
+++ src/main/kotlin/com/weeth/domain/session/domain/repository/SessionRepository.kt
@@ -50,7 +50,6 @@ interface SessionRepository :
         @Param("end") end: LocalDateTime,
     ): List<Session>
 
-    @Query("SELECT s FROM Session s WHERE s.club.id = :clubId AND s.cardinal IN :cardinals")
     fun findByStartLessThanEqualAndEndGreaterThanEqualOrderByStartAsc(
         end: LocalDateTime,
         start: LocalDateTime,
@@ -62,7 +61,7 @@ interface SessionRepository :
     ): List<Session> = findByStartLessThanEqualAndEndGreaterThanEqualOrderByStartAsc(end, start)
 
     override fun findAllByClubIdAndCardinalIn(
-        @Param("clubId") clubId: Long,
-        @Param("cardinals") cardinals: List<Int>,
+        clubId: Long,
+        cardinals: List<Int>,
     ): List<Session>
 }
