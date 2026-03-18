ecf3c53861e9924536b5741812343cdf33ae6654
soo0711 <k300hys@naver.com>
2026-03-18 23:39:43 +0900

    test: QueryService 통합에 맞춰 테스트 수정

--- src/test/kotlin/com/weeth/domain/university/application/usecase/query/GetUniversityQueryServiceTest.kt
+++ src/test/kotlin/com/weeth/domain/university/application/usecase/query/GetUniversityQueryServiceTest.kt
@@ -8,11 +8,11 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.mockk.every
 import io.mockk.mockk
 
-class GetSchoolQueryServiceTest :
+class GetUniversityQueryServiceTest :
     DescribeSpec({
         val universityInfoPort = mockk<UniversityInfoPort>()
         val universityMapper = mockk<UniversityMapper>()
-        val queryService = GetSchoolQueryService(universityInfoPort, universityMapper)
+        val queryService = GetUniversityQueryService(universityInfoPort, universityMapper)
 
         describe("getSchools") {
             context("커리어넷 API 오류 시") {
@@ -20,6 +20,16 @@ class GetSchoolQueryServiceTest :
                     every { universityInfoPort.getSchools() } throws CareerNetApiException()
 
                     shouldThrow<CareerNetApiException> { queryService.getSchools() }
+                }
+            }
+        }
+
+        describe("getMajors") {
+            context("커리어넷 API 오류 시") {
+                it("CareerNetApiException을 전파한다") {
+                    every { universityInfoPort.getMajors() } throws CareerNetApiException()
+
+                    shouldThrow<CareerNetApiException> { queryService.getMajors() }
                 }
             }
         }
