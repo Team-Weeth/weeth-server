35ddb6bff94d8e79c9c4612bdb6c92f601d96426
soo0711 <k300hys@naver.com>
2026-03-18 18:42:36 +0900

    test: 학교/학과 api 테스트 구현

---
+++ src/test/kotlin/com/weeth/domain/university/application/usecase/query/GetMajorQueryServiceTest.kt
@@ -0,0 +1,24 @@
+package com.weeth.domain.university.application.usecase.query
+
+import com.weeth.domain.university.application.exception.CareerNetApiException
+import com.weeth.domain.university.domain.port.CareerNetPort
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.mockk.every
+import io.mockk.mockk
+
+class GetMajorQueryServiceTest :
+    DescribeSpec({
+        val careerNetPort = mockk<CareerNetPort>()
+        val queryService = GetMajorQueryService(careerNetPort)
+
+        describe("getMajors") {
+            context("커리어넷 API 오류 시") {
+                it("CareerNetApiException을 전파한다") {
+                    every { careerNetPort.getMajors() } throws CareerNetApiException()
+
+                    shouldThrow<CareerNetApiException> { queryService.getMajors() }
+                }
+            }
+        }
+    })
