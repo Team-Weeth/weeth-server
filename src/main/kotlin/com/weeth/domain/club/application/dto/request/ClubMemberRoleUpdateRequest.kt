863b4a166c7e0103cddc9136b8a14be1d9d91ff6
hyxklee <ewgt1234@naver.com>
2026-03-19 16:41:33 +0900

    docs: LEAD 관련 설명 추가

--- src/main/kotlin/com/weeth/domain/club/application/dto/request/ClubMemberRoleUpdateRequest.kt
+++ src/main/kotlin/com/weeth/domain/club/application/dto/request/ClubMemberRoleUpdateRequest.kt
@@ -8,6 +8,6 @@ data class ClubMemberRoleUpdateRequest(
     @field:Schema(description = "멤버 ID", example = "1")
     @field:Positive
     val clubMemberId: Long,
-    @field:Schema(description = "변경할 권한", example = "ADMIN")
+    @field:Schema(description = "변경할 권한 (LEAD는 별도 API로 요청해주세요. 또한 LEAD는 사용자 뷰에 보이지 않게 해주세요)", example = "ADMIN")
     val memberRole: MemberRole,
 )
