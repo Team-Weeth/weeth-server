c68ff3c0f6fbbb7c13a05402aa66407f41ce8d8f
soo0711 <k300hys@naver.com>
2026-03-25 13:30:24 +0900

    refactor: 불필요한 락 제거

--- src/main/kotlin/com/weeth/domain/board/domain/repository/PostLikeRepository.kt
+++ src/main/kotlin/com/weeth/domain/board/domain/repository/PostLikeRepository.kt
@@ -2,12 +2,8 @@ package com.weeth.domain.board.domain.repository
 
 import com.weeth.domain.board.domain.entity.Post
 import com.weeth.domain.board.domain.entity.PostLike
-import jakarta.persistence.LockModeType
-import jakarta.persistence.QueryHint
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.jpa.repository.QueryHints
 
 interface PostLikeRepository : JpaRepository<PostLike, Long> {
     fun existsByPostAndUserIdAndIsActiveTrue(
@@ -15,9 +11,6 @@ interface PostLikeRepository : JpaRepository<PostLike, Long> {
         userId: Long,
     ): Boolean
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @QueryHints(QueryHint(name = "jakarta.persistence.lock.timeout", value = "2000"))
-    @Query("SELECT pl FROM PostLike pl WHERE pl.post = :post AND pl.userId = :userId")
     fun findByPostAndUserId(
         post: Post,
         userId: Long,
