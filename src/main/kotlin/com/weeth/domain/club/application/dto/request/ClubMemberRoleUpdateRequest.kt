aceee0697f322b24de733aa6bef5bb78efdd745c
hyxklee <ewgt1234@naver.com>
2026-03-25 19:45:06 +0900

    refactor: 경로 파라미터로 수정

--- src/main/kotlin/com/weeth/domain/club/application/dto/request/ClubMemberRoleUpdateRequest.kt
+++ src/main/kotlin/com/weeth/domain/club/application/dto/request/ClubMemberRoleUpdateRequest.kt
@@ -5,9 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Positive
 
 data class ClubMemberRoleUpdateRequest(
-    @field:Schema(description = "멤버 ID", example = "1")
-    @field:Positive
-    val clubMemberId: Long,
     @field:Schema(description = "변경할 권한 (LEAD는 별도 API로 요청해주세요. 또한 LEAD는 사용자 뷰에 보이지 않게 해주세요)", example = "ADMIN")
     val memberRole: MemberRole,
 )
