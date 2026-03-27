8e525233ed75bb8df7120d1d2f38c25e69d95d01
hyxklee <ewgt1234@naver.com>
2026-03-27 17:19:34 +0900

    refactor: 이번 주 세션 리스트로 반환하도록 수정

--- src/main/kotlin/com/weeth/domain/session/application/dto/response/SessionInfosResponse.kt
+++ src/main/kotlin/com/weeth/domain/session/application/dto/response/SessionInfosResponse.kt
@@ -3,8 +3,8 @@ package com.weeth.domain.session.application.dto.response
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class SessionInfosResponse(
-    @field:Schema(description = "이번 주 정기모임")
-    val thisWeek: SessionInfoResponse?,
+    @field:Schema(description = "이번 주 정기모임 목록")
+    val thisWeek: List<SessionInfoResponse>,
     @field:Schema(description = "반복 그룹 단위 정기모임 목록")
     val sessions: List<SessionGroupResponse>,
 )
