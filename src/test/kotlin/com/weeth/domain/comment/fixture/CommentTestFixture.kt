290abd3d1e2e4881da4277aca426f04b1bf1e1cc
soo0711 <k300hys@naver.com>
2026-04-07 13:25:57 +0900

    refactor: Post/Comment fixture user를 clubMember로 변경

--- src/test/kotlin/com/weeth/domain/comment/fixture/CommentTestFixture.kt
+++ src/test/kotlin/com/weeth/domain/comment/fixture/CommentTestFixture.kt
@@ -1,22 +1,23 @@
 package com.weeth.domain.comment.fixture
 
 import com.weeth.domain.board.domain.entity.Post
+import com.weeth.domain.club.domain.entity.ClubMember
+import com.weeth.domain.club.fixture.ClubMemberTestFixture
 import com.weeth.domain.comment.domain.entity.Comment
-import com.weeth.domain.user.domain.entity.User
 
 object CommentTestFixture {
     fun createPostComment(
         id: Long = 1L,
         content: String = "테스트 댓글",
         post: Post,
-        user: User,
+        clubMember: ClubMember = ClubMemberTestFixture.createActiveMember(),
         parent: Comment? = null,
         isDeleted: Boolean = false,
     ) = Comment(
         id = id,
         content = content,
         post = post,
-        user = user,
+        clubMember = clubMember,
         parent = parent,
         isDeleted = isDeleted,
     )
