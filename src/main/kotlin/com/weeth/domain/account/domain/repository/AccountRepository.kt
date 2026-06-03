d9ea189179b239919bef981fcfd92d4358d50092
hyxklee <ewgt1234@naver.com>
2026-06-03 10:35:58 +0900

    refactor: 락 타임아웃 추가

--- src/main/kotlin/com/weeth/domain/account/domain/repository/AccountRepository.kt
+++ src/main/kotlin/com/weeth/domain/account/domain/repository/AccountRepository.kt
@@ -3,9 +3,11 @@ package com.weeth.domain.account.domain.repository
 import com.weeth.domain.account.domain.entity.Account
 import com.weeth.domain.account.domain.enums.AccountStatus
 import jakarta.persistence.LockModeType
+import jakarta.persistence.QueryHint
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jpa.repository.QueryHints
 import org.springframework.data.repository.query.Param
 
 interface AccountRepository : JpaRepository<Account, Long> {
@@ -32,6 +34,7 @@ interface AccountRepository : JpaRepository<Account, Long> {
     ): Boolean
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints(QueryHint(name = "jakarta.persistence.lock.timeout", value = "2000"))
     @Query("select a from Account a where a.id = :id")
     fun findByIdWithLock(
         @Param("id") id: Long,
