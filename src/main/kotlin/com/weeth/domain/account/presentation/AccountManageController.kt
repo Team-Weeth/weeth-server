76979beaf3c110b12d47be11584589aaf00089dc
hyxklee <ewgt1234@naver.com>
2026-06-11 13:35:31 +0900

    docs: API 명세 수정

--- src/main/kotlin/com/weeth/domain/account/presentation/AccountManageController.kt
+++ src/main/kotlin/com/weeth/domain/account/presentation/AccountManageController.kt
@@ -9,13 +9,17 @@ import com.weeth.global.common.web.TsidParam
 import com.weeth.global.common.web.TsidPathVariable
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "ACCOUNT ADMIN", description = "[ADMIN] 회비 어드민 API")
 @RestController
+@RequestMapping("/api/v4/admin/clubs/{clubId}/accounts")
 class AccountManageController(
     private val manageAccountUseCase: ManageAccountUseCase,
 ) {
