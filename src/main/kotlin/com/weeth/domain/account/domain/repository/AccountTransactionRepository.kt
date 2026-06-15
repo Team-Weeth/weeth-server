c08d89ee8ab9f71e71167b7ba73ef29e75168cfd
hyxklee <ewgt1234@naver.com>
2026-06-15 20:44:17 +0900

    feat: 거래내역 관련 쿼리 메서드 추가

--- src/main/kotlin/com/weeth/domain/account/domain/repository/AccountTransactionRepository.kt
+++ src/main/kotlin/com/weeth/domain/account/domain/repository/AccountTransactionRepository.kt
@@ -1,12 +1,54 @@
 package com.weeth.domain.account.domain.repository
 
 import com.weeth.domain.account.domain.entity.AccountTransaction
+import com.weeth.domain.account.domain.enums.AccountTransactionDirection
 import com.weeth.domain.account.domain.enums.AccountTransactionType
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface AccountTransactionRepository : JpaRepository<AccountTransaction, Long> {
     fun countByAccountIdAndTypeInAndDeletedAtIsNull(
         accountId: Long,
         types: Collection<AccountTransactionType>,
+    ): Long
+
+    fun findByIdAndDeletedAtIsNull(id: Long): AccountTransaction?
+
+    // 목록 조회 (소프트 삭제 제외)
+    fun findByAccountIdAndDeletedAtIsNull(
+        accountId: Long,
+        pageable: Pageable,
+    ): Page<AccountTransaction>
+
+    fun findByAccountIdAndTypeAndDeletedAtIsNull(
+        accountId: Long,
+        type: AccountTransactionType,
+        pageable: Pageable,
+    ): Page<AccountTransaction>
+
+    fun findByAccountIdAndTypeInAndDeletedAtIsNull(
+        accountId: Long,
+        types: Collection<AccountTransactionType>,
+        pageable: Pageable,
+    ): Page<AccountTransaction>
+
+    fun findByAccountIdAndDirectionAndDeletedAtIsNull(
+        accountId: Long,
+        direction: AccountTransactionDirection,
+        pageable: Pageable,
+    ): Page<AccountTransaction>
+
+    // 필터 탭 카운트
+    fun countByAccountIdAndDeletedAtIsNull(accountId: Long): Long
+
+    fun countByAccountIdAndTypeAndDeletedAtIsNull(
+        accountId: Long,
+        type: AccountTransactionType,
+    ): Long
+
+    fun countByAccountIdAndDirectionAndDeletedAtIsNull(
+        accountId: Long,
+        direction: AccountTransactionDirection,
     ): Long
 }
